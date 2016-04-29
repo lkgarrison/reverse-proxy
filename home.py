160a527e922779ae9242b1f7cd76b0a72072570c
Luke Garrison <lgarriso@nd.edu>
2016-04-28 23:34:50 -0400

    Supports multiple ssh logins, code cleanup

--- home.py
+++ home.py
@@ -7,87 +7,73 @@ from twisted.internet.tcp import Port
 from twisted.internet import reactor
 from twisted.internet.defer import DeferredQueue
 
-# queue = DeferredQueue()
-# queue2 = DeferredQueue()
-
-COMMAND_CONN_PORT = 32001
-DATA_CONN_PORT = 32002
+COMMAND_CONN_PORT = 40212
+DATA_CONN_PORT = 42212
 SSH_PORT = 9007
 
-SERVER_HOST = "student02.cse.nd.edu"
-SERVER_PORT = 40001
-
-
 class HomeClientConnection(Protocol):
-    def __init__(self, addr, homeWorkConnection):
+    def __init__(self, addr, homeWorkCommandConnection):
         self.addr = addr
-        self.homeWorkConnection = homeWorkConnection
+        self.homeWorkCommandConnection = homeWorkCommandConnection
         self.deferredQueue = DeferredQueue()
         print "connection received from", addr
 
     def startForwarding(self, homeWorkDataConnection):
-        print "start forwarding"
         self.homeWorkDataConnection = homeWorkDataConnection
         self.deferredQueue.get().addCallback(self.sendData)
     
     def sendData(self, data):
-        print "sendData called"
         self.homeWorkDataConnection.transport.write(data)
         self.deferredQueue.get().addCallback(self.sendData)
 
     def connectionMade(self):
-        self.homeWorkConnection.createHomeWorkDataConnection(self)
+        self.homeWorkCommandConnection.createHomeWorkDataConnection(self)
 
     def dataReceived(self, data):
         self.deferredQueue.put(data)
-        print "data received here"
-        print data
 
     def connectionLost(self, reason):
         print "connection lost from ", self.addr
 
 class HomeClientConnectionFactory(Factory):
-    def __init__(self, homeWorkConnection):
-        self.homeWorkConnection = homeWorkConnection
+    def __init__(self, homeWorkCommandConnection):
+        self.homeWorkCommandConnection = homeWorkCommandConnection
 
     def buildProtocol(self, addr):
-        return HomeClientConnection(addr, self.homeWorkConnection)
+        return HomeClientConnection(addr, self.homeWorkCommandConnection)
 
 
-class HomeWorkConnection(Protocol):
+class homeWorkCommandConnection(Protocol):
     def __init__(self, addr):
         # save addr for later use
         self.addr = addr
         print "connection received from", addr
-        # reactor.connectTCP(SERVER_HOST, SERVER_PORT, ProxyClientFactory());
         reactor.listenTCP(SSH_PORT, HomeClientConnectionFactory(self))
 
     def dataReceived(self, data):
-        # queue2.put(data)
-        # queue.get().addCallback(self.sendResponseToClient)
-        print data
+        if data == "drop data connection":
+            self.dataConnection.stopListening()
+        pass
 
     def connectionLost(self, reason):
         print "connection lost from", self.addr
-        # reactor.stop()
 
     def createHomeWorkDataConnection(self, HC):
         self.HC = HC
 
         # start listening for data coming from the data connection
-        reactor.listenTCP(DATA_CONN_PORT, HomeWorkDataConnectionFactory(HC))
+        self.dataConnection = reactor.listenTCP(DATA_CONN_PORT, HomeWorkDataConnectionFactory(HC))
 
         # start data connection on work computer
         self.transport.write("start data connection")
 
-class HomeWorkConnectionFactory(Factory):
+class homeWorkCommandConnectionFactory(Factory):
     def buildProtocol(self, addr):
-        return HomeWorkConnection(addr)
+        return homeWorkCommandConnection(addr)
 
 
 class HomeWorkDataConnection(Protocol):
     def __init__(self, addr, homeClientConnection):
-        print "listening for data connection"
         self.addr = addr
         self.homeClientConnection = homeClientConnection
 
@@ -97,7 +83,6 @@ class HomeWorkDataConnection(Protocol):
     def dataReceived(self, data):
         # forward data to homeClientConnection
         self.homeClientConnection.transport.write(data)
-        print "here 4"
 
     def connectionLost(self, reason):
         print "connection lost from", self.addr
@@ -110,7 +95,7 @@ class HomeWorkDataConnectionFactory(Factory):
         return HomeWorkDataConnection(addr, self.homeClientConnection)
 
 
-reactor.listenTCP(COMMAND_CONN_PORT, HomeWorkConnectionFactory())
+reactor.listenTCP(COMMAND_CONN_PORT, homeWorkCommandConnectionFactory())
 
 reactor.run()
 
--- work.py
+++ work.py
@@ -7,8 +7,10 @@ from twisted.internet.protocol import Protocol, ClientFactory
 from twisted.internet.defer import DeferredQueue
 
 HOME_ADDRESS = "student02.cse.nd.edu"
-COMMAND_PORT = 32001
-DATA_PORT = 32002
+COMMAND_PORT = 40212
+DATA_PORT = 42212
+
+SSH_ADDRESS = "student02.cse.nd.edu"
 SSH_PORT = 22 # the "service"
 
 class HomeWorkCommandConnection(Protocol):
@@ -16,15 +18,17 @@ class HomeWorkCommandConnection(Protocol):
         print "new connection made to", HOME_ADDRESS, "port", COMMAND_PORT
         
     def dataReceived(self, data):
-        print "received data", data
         if data == "start data connection":
-            print "starting data connection"
-            reactor.connectTCP(HOME_ADDRESS, DATA_PORT, HomeWorkDataConnectionFactory())
+            reactor.connectTCP(HOME_ADDRESS, DATA_PORT, HomeWorkDataConnectionFactory(self))
         return
+
+    def dropDataConnection(self):
+        # home.py is listening for this command. If it receives it, it will stop listening
+        # on the data port
+        self.transport.write("drop data connection")
 
     def connectionLost(self, reason):
         print "lost connection to", HOME_ADDRESS, "port", COMMAND_PORT
-        # reactor.stop()
 
 
 class HomeWorkCommandConnectionFactory(ClientFactory):
@@ -34,24 +38,31 @@ class HomeWorkCommandConnectionFactory(ClientFactory):
 
 
 class HomeWorkDataConnection(Protocol):
+    def __init__(self, homeWorkCommandConnection):
+        self.homeWorkCommandConnection = homeWorkCommandConnection
+
     def connectionMade(self):
         print "new connection made to", HOME_ADDRESS, "port", COMMAND_PORT
         # initialize connection to service/ssh
-        reactor.connectTCP(HOME_ADDRESS, SSH_PORT, WorkServiceConnectionFactory(self))
+        reactor.connectTCP(SSH_ADDRESS, SSH_PORT, WorkServiceConnectionFactory(self))
         self.deferredQueue = DeferredQueue()
 
     def dataReceived(self, data):
-        print "received data", data
         self.deferredQueue.put(data)
         return
 
     def connectionLost(self, reason):
         print "lost connection to", HOME_ADDRESS, "port", COMMAND_PORT
-        # reactor.stop()
+
+    def dropConnection(self):
+        self.homeWorkCommandConnection.dropDataConnection()
 
 class HomeWorkDataConnectionFactory(ClientFactory):
+    def __init__(self, homeWorkCommandConnection):
+        self.homeWorkCommandConnection = homeWorkCommandConnection
+
     def buildProtocol(self, addr):
-        return HomeWorkDataConnection()
+        return HomeWorkDataConnection(self.homeWorkCommandConnection)
 
 
 
@@ -64,10 +75,7 @@ class WorkServiceConnection(Protocol):
         self.homeWorkDataConnection.deferredQueue.get().addCallback(self.sendData)
 
     def dataReceived(self, data):
-        # print "received data", data
         # send data received from ssh back to home
-        print "data received from ssh"
-        print data
         self.homeWorkDataConnection.transport.write(data)
         return
 
@@ -77,8 +85,7 @@ class WorkServiceConnection(Protocol):
         self.homeWorkDataConnection.deferredQueue.get().addCallback(self.sendData)
 
     def connectionLost(self, reason):
-        print "lost connection to", HOME_ADDRESS, "port", SSH_PORT
-        # reactor.stop()
+        self.homeWorkDataConnection.dropConnection()
 
 class WorkServiceConnectionFactory(ClientFactory):
     def __init__(self, homeWorkDataConnection):
